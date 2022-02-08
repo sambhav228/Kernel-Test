a5e120e7cdea31743c20ef72465fa094a844698f
Sambhav Kumar Thakur <sambhavkumar228@gmail.com>
2022-02-08 20:36:31 +0530

    updated

--- ci/centos.sh
+++ ci/centos.sh
@@ -1,1 +1,12 @@
-fedora.sh
+#define PARAM_MSDELAY 300
+#define PARAM_GFPFLAGS GFP_HIGHUSER_MOVABLE
+#define PARAM_ALLOCS 300
+#define PARAM_ORDER 7
+
+function alloc_runtest() %{
+	unsigned long order;		/* Order of pages */
+	unsigned long numpages;		/* Number of pages to allocate */
+	struct page **pages = NULL;	/* Pages that were allocated */
+	unsigned long attempts=0, printed=0;
+	unsigned long alloced=0;
+  }
